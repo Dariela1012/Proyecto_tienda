e23131326c632b80a7000a4f22116eac2a897e2c
Karen Dariela Abarca Jaramillo <karendariela12@gmail.com>
2025-09-06 22:49:50 -0500

    Día 11: Se agregó código en clase Producto.java

--- store-web-main/src/main/java/com/example/store_web/model/Producto.java
+++ store-web-main/src/main/java/com/example/store_web/model/Producto.java
@@ -11,6 +11,14 @@ public class Producto {
     @Column(name ="id_producto")
     private Integer idProducto;
 
-    @Column(name ="nombre", nullable = false. length = 100)
+    @Column(name ="nombre", nullable = false, length = 100)
     private String nombre;
+
+    @Column(name = "descripcion", columnDefinition = "TEXT")
+    private String descripcion;
+
+    @Column(name = "precio", nullable = false, precision = 10)
+    private Double precio;
+
+
 }
